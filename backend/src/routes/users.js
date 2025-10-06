a6f14886de648abc1337cc2681083ca6559f50d6
Davi Alves Rodrigues <davialvesr18@gmail.com>
2025-10-06 08:58:40 -0300

    Delete

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -9,6 +9,13 @@ users_router.get("/", async (req, res) => {
     const { success, statusCode, body } = await users_controller.get_users();
 
     res.status(statusCode).send({ success, statusCode , body });
-})
+});
+
+users_router.delete("/:id", async (req, res) => {
+    console.log(req.params.id);
+    const { success, statusCode, body } = await users_controller.delete_user(req.params.id);
+
+    res.status(statusCode).send({ success, statusCode , body });
+});
 
 export default users_router;
